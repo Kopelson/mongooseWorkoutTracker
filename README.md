1afd3e010b04779521771892b4cc93a3774aeffc
Kenneth Kopelson <kennethkopelson@gmail.com>
2021-01-13 10:21:23 -0800

    added comments, added description to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# mongooseWorkoutTracker
+# Mongoose Workout Tracker
+
+## Overview
+    This is a full-stack workout tracker application. Uses a Mongo database with a Mongoose schema and handle routes with Express.
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 //create an instance of express
 const app = express();
 //use a logger on the server
@@ -11,15 +10,11 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-
+//require routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 //setup database URL and collection name
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
-
-
 // Listen on port 3000
 app.listen(process.env.PORT || 3000, () => {
     console.log("App running on port 3000!");
